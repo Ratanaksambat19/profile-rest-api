28b0ff5bf2033e67e60c82235a86f08642f31321
Ratanaksambat19 <doung.ratanaksambat19@kit.edu.kh>
2020-08-30 11:02:09 +0700

    finalize

--- profile_project/profile_api/urls.py
+++ profile_project/profile_api/urls.py
@@ -4,8 +4,10 @@ from profile_api import views
 
 router = DefaultRouter()
 router.register(r'hello-viewset', views.HelloViewSet, basename = 'hello-viewset')
-
+router.register(r'profile', views.UserProfileViewSet)
+router.register(r'feed', views.UserProfileFeedViewSet   )
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view(), name = 'hello-view'),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('login/', views.UserLoginApiView.as_view(), name = 'login'),
 ]
--- profile_project/profile_api/views.py
+++ profile_project/profile_api/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
-from profile_api import serializers
-
+from profile_api import serializers, models, permissions
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from rest_framework.filters import SearchFilter
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -84,3 +88,31 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk = None):
         """Handle removing object"""
         return Response({'http_method': "DELETE"})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ['name', 'email',]
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handle creating, reading, and updateing profile feed item"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticatedOrReadOnly
+    )
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile = self.request.user)
